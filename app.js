b90b1e8ddb53a7fdc4dbece1cae2f2ad83b8734b
aiksawyer <>
2018-11-08 12:13:52 -0500

    changes

--- app.js
+++ app.js
@@ -12,15 +12,84 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 ///hide apiKey later
-const apiKey = '***************';
+const apiKey = 'wNNhHJV0lMfl1r1nyXaQr4iB6TeqqwO5';
 console.log(apiKey)
+
+// sign up page here
+app.get('/', function(req, res){
+  res.render('signup', {success: null, error: null});
+});
+
+app.post('/', function(req, res){
+  let username = req.body.username;
+  let password = req.body.password;
+  let confirmPass = req.body.confirmPass;
+  let confirmUser = false;
+
+  for (let i=0; i<userData.length; i++){
+    if (userData[i].username === username){
+      confirmUser = true;  
+    }
+  }
+
+  if (confirmUser){
+    res.render('signup', {success: null, error: 'Username already exists, try again!'});
+  }else{
+    if (password===confirmPass){
+      userData.push({username: username, password: password});
+      let finalData = JSON.stringify(userData);
+      fs.writeFile('users.json', finalData, finished);
+      function finished(err) {
+        console.log('file written: '+req.body);
+      }
+
+      // render success!
+      res.render('signup', {success: "Welcome, new user!"});
+    }
+    else{
+      res.render('signup', {success: null, error: "Passwords have to be the same!"});
+    }   
+  }
+});
+
+// log in page here
+app.get('/log', function(req, res){
+  res.render('log', {error: null});
+});
+
+app.post('/log', function(req, res){
+  let username = req.body.username;
+  let password = req.body.password;
+
+  let gotUser = false;
+  let userIndex = null;
+
+  ///check for user and password
+  for (let i=0; i<userData.length; i++){
+    if (userData[i].username === username){
+      gotUser = true;
+      userIndex = i;
+    }
+  }
+
+  if (gotUser){
+    if (password === userData[userIndex].password){
+      res.redirect('/vote');
+    }else{ 
+      res.render('log', {error: 'Incorrect password, try again!'});
+    }
+  }else{
+    res.render('log', {error: 'User not found'});
+  }
+
+});
 
 app.get('/', function (req, res) {
   res.render('index', {gifUrl: null, error: null});
 })
 
 
-
+////the main page here
 app.post('/', function (req, res) {
   let search = req.body.search;
   ///user's apiKey goes here
